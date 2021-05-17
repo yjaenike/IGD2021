5ef9945a4e898a17aecc57617bd48913d9db98bb
yjaenike <38246619+yjaenike@users.noreply.github.com>
2021-05-17 10:09:59 -0700

    added player detection
    added player detection

---
+++ Assets/Scripts/playerDetection.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class playerDetection : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public BoxCollider bc;
+    public MeshRenderer mr;
+    public int damage = 0;
+
+
+    void OnCollisionStay (Collision col)
+    {
+        if (col.collider.tag == "Player")
+        {   
+            damage = damage + 1;
+
+
+            // activate gravity
+            if (damage == 40)
+            {
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezePositionX;
+                rb.constraints = RigidbodyConstraints.FreezePositionZ;
+
+                Invoke("removePlatform", 2.0f);
+
+            }
+        }
+    }
+
+    private void removePlatform()
+    {
+        bc.enabled = !bc.enabled;
+        mr.enabled = !mr.enabled;
+    }
+}
