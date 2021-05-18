937679936e454235b356313791df6e8372097ac5
Marcel Robohm <internetzverbindung@gmail.com>
2021-05-18 18:37:56 +0200

    features

--- Assets/Scripts/playerDetection.cs
+++ Assets/Scripts/playerDetection.cs
@@ -1,37 +1,64 @@
-﻿using UnityEngine;
+﻿using System;
 
-public class playerDetection : MonoBehaviour
+using UnityEngine;
+
+enum PlatformState { Virgin, Dying, Dead }
+
+public class PlayerDetection : MonoBehaviour
 {
 
     public Rigidbody rb;
     public BoxCollider bc;
     public MeshRenderer mr;
-    public int damage = 0;
+    public float decaySpeed = 0.2f;
+
+    private float decay = 0.0f; // [0.0, 1.0]
+
+    private PlatformState state = PlatformState.Virgin;
 
 
     void OnCollisionStay (Collision col)
     {
-        if (col.collider.tag == "Player")
-        {   
-            damage = damage + 1;
+        if (!col.collider.CompareTag("Player")) return;
 
+        var myDelta = Time.deltaTime * decaySpeed;
+        decay += myDelta;
+        Debug.Log(decay);
 
-            // activate gravity
-            if (damage == 40)
-            {
-                rb.constraints = RigidbodyConstraints.None;
-                rb.constraints = RigidbodyConstraints.FreezePositionX;
-                rb.constraints = RigidbodyConstraints.FreezePositionZ;
+        var fallingDistance = myDelta * 2.0f * (float)Math.Pow(decay, 3.0f);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - fallingDistance, this.transform.position.z);
 
-                Invoke("removePlatform", 2.0f);
+        CalculateNewState();
+        SetStateDependentColor();
+        //SetStateDependentPhysics();
 
-            }
+    }
+
+    void CalculateNewState() {
+        state = (decay >= 1.0) ? PlatformState.Dead : PlatformState.Dying;
+    }
+
+    void SetStateDependentColor() {
+        switch (state) {
+            case PlatformState.Virgin:
+                this.GetComponent<MeshRenderer>().material.color = new Color(0.2f, 0.2f, 0.2f, 1.0f);
+                break;
+            case PlatformState.Dying:
+                // The rottier the more red
+                var newColor = new Color(0.2f + (decay * 0.8f), 0.2f - (decay * 0.2f), 0.2f - (decay * 0.2f), 1.0f);
+                this.GetComponent<MeshRenderer>().material.color = newColor;
+                break;
+            case PlatformState.Dead:
+                // make platform unvisible
+                Debug.Log("DEAD");
+                this.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+                break;
         }
     }
 
-    private void removePlatform()
-    {
-        bc.enabled = !bc.enabled;
-        mr.enabled = !mr.enabled;
+    void SetStateDependentPhysics() {
+        var isAlive = state != PlatformState.Dead;
+        bc.enabled = isAlive;
+        mr.enabled = isAlive;
     }
 }
